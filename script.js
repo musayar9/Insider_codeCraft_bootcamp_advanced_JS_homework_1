dd1477b880fa0877166d11e30e7d91e2368c6d58
Musa <musasayar67@gmail.com>
2025-07-23 02:07:50 +0300

    fetch users and store them in localStorage with expiresDate

--- script.js
+++ script.js
@@ -25,9 +25,66 @@
     `);
 
     $("body").html(`
-
+            <div class="ins-api-users"></div>
 
       `);
+      
+      
+      
+      
+       let users = JSON.parse(localStorage.getItem("users"));
+    // const expiresDate = new Date().getTime() + 1000 * 60 * 60 * 24;
+    const expiresDate = new Date().setHours(new Date().getHours() + 24);
+    const nowDate = new Date().getTime();
+    console.log("date", new Date(expiresDate));
+    console.log("expires", expiresDate);
+
+    getData();
+
+    function getData() {
+      if (users) {
+        if (nowDate < users.expiresDate) {
+          console.log("users bylundu", users.data);
+        } else {
+          console.log("suresi dolmuş userların");
+          fetchUsers();
+        }
+      } else {
+        console.log("users bulunamadı");
+        fetchUsers();
+      }
+    }
+
+    async function fetchUsers() {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await res.json();
+        if (res.status === 404) {
+          throw new Error("404 Not Found");
+        } else if (res.status === 500) {
+          throw new Error("500 Internal Server Error");
+        } else {
+          console.log("data", data);
+          localStorage.setItem(
+            "users",
+            JSON.stringify({
+              data,
+              expiresDate,
+            })
+          );
+        }
+      } catch (error) {
+        console.log("error");
+      }
+    }
+      
+      
+      
+      
+      
+      
+      
+      
   } catch (err) {
     console.log("err", err);
   }
